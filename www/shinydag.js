48e00709268a210b22d517c5b961a28dc743b899
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2019-07-01 18:01:55 +0000

    Block problematic inputs during re-render or updating
    Block off node selection buttons when user updates node label but before Shiny has a chance to update the UI
    
    Don't allow typing when Shiny is rendering, i.e. can't change node name if waiting on Shiny to report back

--- www/shinydag.js
+++ www/shinydag.js
@@ -1,16 +1,16 @@
 const toggle_reset_icon = (v = false, id) => {
-  const icon = document.getElementById(id).getElementsByTagName("i")[0]
+  const icon = document.getElementById(id).getElementsByTagName("i")[0];
   if (v) {
-    icon.className = "fa fa-backspace"
+    icon.className = "fa fa-backspace";
   } else {
-    icon.className = "fa fa-plus"
+    icon.className = "fa fa-plus";
   }
 };
 
 const set_input_focus = (id) => {
-  const el = document.getElementById(id)
+  const el = document.getElementById(id);
   if (el) {
-    el.focus()
+    el.focus();
   }
 };
 
@@ -36,4 +36,30 @@ $( document ).ready(function() {
   setTimeout(append_edge_selector_hint, 1000);
   setTimeout(function() {wrap_btn_text_in_span("downloadButton", "Download")}, 1000);
   setTimeout(function() {wrap_btn_text_in_span("\\._bookmark_", "Bookmark")}, 1000);
+});
+
+// Block name change updates while Shiny is re-rendering to avoid wonkiness
+var text_input_timeout;
+$(document).on("shiny:busy", (e) => { 
+  text_input_timeout = setTimeout(() => {
+    $("#node_list_node_name").prop("disabled", true);
+  }, 500);
+});
+$(document).on("shiny:idle", (e) => { 
+  clearTimeout(text_input_timeout);
+  $("#node_list_node_name").prop("disabled", false);
+});
+
+// Block node change buttons when updating names to avoid infinite looping wonkiness
+// disables buttons when user starts typing in text box
+$("#node_list_node_name").keydown(() => { 
+  $("#nodeListButtons button").prop("disabled", true);
+});
+
+// re-enable buttons when Shiny updates or the text bar loses focus (in case no change)
+$(document).on("shiny:value", () => { 
+  $("#nodeListButtons button").prop("disabled", false);
+});
+$("#node_list_node_name").blur(() => { 
+  $("#nodeListButtons button").prop("disabled", false);
 });
