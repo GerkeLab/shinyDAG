b3742ade52b85a889b3134de2cd552cbc436536e
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2019-07-01 20:08:48 +0000

    Use triange instead of square to indicate parent node
    and replace unicode shape hints with dynamically generated and inserted SVG elements

--- www/shinydag.js
+++ www/shinydag.js
@@ -14,14 +14,51 @@ const set_input_focus = (id) => {
   }
 };
 
+// Helper function to create SVG elements
+// ref: https://stackoverflow.com/a/22555293
+var svgNS = "http://www.w3.org/2000/svg";
+const new_svg = width => {
+  var svg_el = document.createElementNS(svgNS, "svg");
+  svg_el.setAttributeNS(null, "viewBox", "0 0 100 100");
+  svg_el.setAttributeNS(null, "width", width);
+  return svg_el;
+};
+
+const circle_svg = () => {
+  // create a simple SVG circle shape
+  var svg = new_svg("15px");
+  var circle = document.createElementNS(svgNS, "circle");
+  circle.setAttributeNS(null, "id", "edge-selector-hint-circle");
+  circle.setAttributeNS(null, "cx", "50%");
+  circle.setAttributeNS(null, "cy", "50%");
+  circle.setAttributeNS(null, "r", "45%");
+  circle.setAttributeNS(null, "fill", "#ddd");
+  circle.setAttributeNS(null, "stroke", "#999");
+  svg.appendChild(circle);
+  return svg;
+};
+
+const triangle_svg = () => {
+  // create a simple SVG triangle shape
+  var svg = new_svg("15px");
+  var triangle = document.createElementNS(svgNS, "polygon");
+  triangle.setAttributeNS(null, "id", "edge-selector-hint-triangle");
+  triangle.setAttributeNS(null, "points", "0,100 50,10 100,100");
+  triangle.setAttributeNS(null, "fill", "#ddd");
+  triangle.setAttributeNS(null, "stroke", "#999");
+  svg.appendChild(triangle);
+  return svg;
+};
+
 const append_edge_selector_hint = () => {
+  // Add shape hints to edge selection labels because can't from inside Shiny
   var helper_from = document.createElement("span");
-  helper_from.innerHTML = "&#x25fc;";
-  helper_from.className = "edge-selector-hint-from";
+  helper_from.appendChild(triangle_svg());
+  helper_from.className = "edge-selector-hint";
   
   var helper_to = document.createElement("span");
-  helper_to.innerHTML = "&#x2b24;";
-  helper_to.className = "edge-selector-hint-to";
+  helper_to.appendChild(circle_svg());
+  helper_to.className = "edge-selector-hint";
   
   $("label[for='from_edge-selectized']").after(helper_from);
   $("label[for='to_edge-selectized']").after(helper_to);
